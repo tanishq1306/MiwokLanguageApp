b662e1656a4dc8837c71d3a351fff5d0acc1327e
Tanishq Mahendra Saini <52647176+tanishq1306@users.noreply.github.com>
2021-01-02 23:27:02 +0530

    Lesson 1 implemented Created activities and xml files

---
+++ app/src/main/java/com/example/miwoklanguageapp/ColorsActivity.java
@@ -0,0 +1,14 @@
+package com.example.miwoklanguageapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ColorsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colors);
+    }
+}
---
+++ app/src/main/java/com/example/miwoklanguageapp/PhrasesActivity.java
@@ -0,0 +1,13 @@
+package com.example.miwoklanguageapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PhrasesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phrases);
+    }
+}
