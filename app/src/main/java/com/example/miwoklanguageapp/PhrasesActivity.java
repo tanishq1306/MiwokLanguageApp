3fd03d9496e24e4b42aa8b32a588bbd1db0f5c7d
Tanishq Mahendra Saini <52647176+tanishq1306@users.noreply.github.com>
2021-01-03 22:21:14 +0530

    Lesson 3 implemented version 1.0 Added Image and updated xml and activities accordingly.

--- app/src/main/java/com/example/miwoklanguageapp/PhrasesActivity.java
+++ app/src/main/java/com/example/miwoklanguageapp/PhrasesActivity.java
@@ -29,7 +29,7 @@ public class PhrasesActivity extends AppCompatActivity {
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
-        WordAdapter adapter = new WordAdapter(this, words);
+        WordAdapter adapter = new WordAdapter(this, words, R.color.category_phrases);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
@@ -38,5 +38,6 @@ public class PhrasesActivity extends AppCompatActivity {
 
         // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
         // {@link ListView} will display list items for each {@link Word} in the list.
-        listView.setAdapter(adapter);    }
+        listView.setAdapter(adapter);
+    }
 }
